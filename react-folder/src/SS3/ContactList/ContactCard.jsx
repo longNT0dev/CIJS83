2f7baa799556a53ded95e933ab91d68658ef475c
LongNTDev <codeinlife10@example.com>
2023-04-22 22:17:14 +0700

    ok

--- react-folder/src/SS3/ContactList/ContactCard.jsx
+++ react-folder/src/SS3/ContactList/ContactCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function ContactCard({ avatarUrl, name, lastMessage }) {
     return (
         <div style={{ display: 'flex' }}>
-            <img src={avatarUrl} style={{ borderRadius: '50%' }} />
+            <img src={avatarUrl} style={{ width: '30px', height: '30px', borderRadius: '50%' }} />
 
             <div>
                 <h2>{name}</h2>
--- react-folder/src/SS3/ContactList/ContactList.jsx
+++ react-folder/src/SS3/ContactList/ContactList.jsx
@@ -1,53 +1,100 @@
 import React, { useState } from 'react'
 import IconSizeBigger from '../shared/IconSizeBigger'
 import ContactCard from './ContactCard'
+import { v4 as uuidv4 } from 'uuid';
 
 function ContactList() {
-
+  
   const [contacts, setContacts] = useState([
     {
       id: 1,
       name: 'John Doe',
-      avatarUrl: 'fas fa-user',
+      avatarUrl: 'https://www.seiu1000.org/sites/main/files/main-images/camera_lense_0.jpeg',
       lastMessage: ''
     },
     {
       id: 2,
       name: 'John Doe',
-      avatarUrl: 'fas fa-user',
+      avatarUrl: 'https://www.seiu1000.org/sites/main/files/main-images/camera_lense_0.jpeg',
       lastMessage: ''
     },
     {
       id: 3,
       name: 'John Doe',
-      avatarUrl: 'fas fa-user',
+      avatarUrl: 'https://www.seiu1000.org/sites/main/files/main-images/camera_lense_0.jpeg',
       lastMessage: ''
     },
     {
       id: 4,
       name: 'John Doe',
-      avatarUrl: 'fas fa-user',
+      avatarUrl: 'https://www.seiu1000.org/sites/main/files/main-images/camera_lense_0.jpeg',
       lastMessage: ''
     },
     {
       id: 5,
       name: 'John Doe',
-      avatarUrl: 'fas fa-user',
+      avatarUrl: 'https://www.seiu1000.org/sites/main/files/main-images/camera_lense_0.jpeg',
       lastMessage: 'hi'
     },
     {
       id: 6,
       name: 'John Doe',
-      avatarUrl: 'fas fa-user',
+      avatarUrl: 'https://www.seiu1000.org/sites/main/files/main-images/camera_lense_0.jpeg',
       lastMessage: 'hi'
     },
     {
       id: 7,
       name: 'John Doe',
-      avatarUrl: 'fas fa-user',
+      avatarUrl: 'https://www.seiu1000.org/sites/main/files/main-images/camera_lense_0.jpeg',
       lastMessage: ''
     },
   ])
+
+  const [userInput, setUserInput] = useState({
+    id: '',
+    name: '',
+    avatarUrl: '',
+    lastMessage: ''
+  })
+
+
+  const addNewContact = (newContact) => {
+    // Tạo 1 mảng mới và thêm 1 giá trị vào ban đầu 
+    setContacts([...contacts, newContact])
+
+  }
+
+  const addNewContact2 = () => {
+    // Kiểm tra các giá trị userInput có ok k 
+    if (false) return
+
+    let newContact = {
+      id: uuidv4(),
+      name: userInput.name,
+      avatarUrl: 'https://www.seiu1000.org/sites/main/files/main-images/camera_lense_0.jpeg',
+      lastMessage: userInput.lastMessage
+    }
+
+    // Tạo 1 mảng mới và thêm 1 giá trị vào ban đầu 
+    setContacts([...contacts, newContact])
+  }
+
+  // const handleUsernameChange = (e) => {
+  //   let username = e.target.value
+
+  //   setUserInput({ ...userInput, name: username })
+  //   // setUserInput(Object.assign(userInput, { name: username }))
+  // }
+
+  // const handleLastMessageChange = (e) => {
+  //   let lastMessage = e.target.value
+
+  //   setUserInput({ ...userInput, lastMessage: lastMessage })
+  // }
+
+  const handleDataChange = (e) => {
+    setUserInput({ ...userInput, [e.target.name]: e.target.value })
+  }
 
   return (
     <div style={{ width: '30%', display: 'flex', rowGap: '12px', flexDirection: 'column', padding: '12px' }}>
@@ -55,9 +102,9 @@ function ContactList() {
       <div style={{ display: 'flex', justifyContent: 'space-between' }}>
         <h3>Chats</h3>
 
-        <div style={{ display: 'flex', columnGap: '8px' }}>
+        <div style={{ display: 'flex', columnGap: '8px', alignItems: 'center' }}>
           <IconSizeBigger className="fa-regular fa-comment" size="16px"></IconSizeBigger>
-          <IconSizeBigger className="fa-regular fa-comment" size="16px"></IconSizeBigger>
+          <div style={{ fontSize: '20px', cursor: 'pointer' }} onClick={() => addNewContact2()}>+</div>
         </div>
       </div>
 
@@ -68,7 +115,7 @@ function ContactList() {
         <button style={{ color: 'black' }}>Unread</button>
       </div>
 
-      <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', height: '80%', overflowY: 'auto' }}>
 
         {
           contacts.map((contact, index) => (
@@ -77,6 +124,11 @@ function ContactList() {
         }
 
       </div>
+      {/* 
+      <input type="text" name="username" onChange={(e) => handleUsernameChange(e)} />
+      <input type="text" name="lastMessage" onChange={handleLastMessageChange} /> */}
+      <input type="text" name="name" onChange={handleDataChange} />
+      <input type="text" name="lastMessage" onChange={handleDataChange} />
     </div>
   )
 }
