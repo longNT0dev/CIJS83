0a24473bbe7efd58759f8c031260f93e382bf064
LongNTDev <codeinlife10@example.com>
2023-05-09 22:16:41 +0700

    ok

--- react-folder/src/SS3/ContactList/ContactCard.jsx
+++ react-folder/src/SS3/ContactList/ContactCard.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function ContactCard({ id, avatarUrl, name, lastMessage }) {
-    return (
 
-        <a href={`/?chatId=${id}`} style={{ textDecoration: 'none' }}>
+    const handleDeleteMessages = (e, chatId) => {
+        e.stopPropagation()
+        e.preventDefault()
+        fetch(`https://64565c532e41ccf1691ad14d.mockapi.io/contact-list/${chatId}`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                messages: []
+            })
+        })
+    }
+
+    return (
+        <Link to={`/?chatId=${id}`} style={{ textDecoration: 'none' }}>
             <div style={{ display: 'flex' }}>
                 <img src={avatarUrl} style={{ width: '30px', height: '30px', borderRadius: '50%' }} />
 
@@ -12,8 +25,9 @@ function ContactCard({ id, avatarUrl, name, lastMessage }) {
                     <h3>{lastMessage}</h3>
                 </div>
             </div>
-        </a>
 
+            <button onClick={(e) => handleDeleteMessages(e, id)}>😒</button>
+        </Link>
     )
 }
 
--- react-folder/src/SS3/MessageContainer/MessageContainer.jsx
+++ react-folder/src/SS3/MessageContainer/MessageContainer.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { useLocation, useParams } from 'react-router-dom'
 
 
 function MessageContainer() {
+  let location = useLocation();
 
-  const url = new URL(window.location);
-  const searchParams = url.searchParams;
+  useEffect(() => {
+    const url = new URL(window.location);
+    const searchParams = url.searchParams;
 
-  searchParams.get('chatId');
+    const id = searchParams.get('chatId');
+
+    async function fetchData(chatId) {
+      let response = await fetch(`https://64565c532e41ccf1691ad14d.mockapi.io/contact-list/${chatId}`)
+      let result = await response.json()
+
+
+      console.log(result)
+    }
+
+    fetchData(id)
+
+  }, [location])
+
 
   return (
     <div style={{ width: '40%' }}>MessageContainer</div>
